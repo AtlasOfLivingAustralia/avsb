3322e9c4a00803ad6f0e0992cb5f7d2f909c0772
Jack Brinkman <jack.brinkman@csiro.au>
2023-03-06 14:19:47 +1000

    Improved UX of Contact card on smaller screens

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,16 +1,27 @@
-import { PropsWithChildren, ReactElement } from 'react';
+import { CSSProperties } from 'react';
 import { Anchor, Avatar, Badge, Group, Paper, Skeleton, Stack, Text } from '@mantine/core';
-import { IconMail, IconPhone, IconMapPin } from '@tabler/icons';
+import { IconMail, IconPhone, IconMapPin, IconBuilding } from '@tabler/icons';
 import { useGQLQuery } from '#/api';
 import { Contact as ContactType } from '#/api/graphql/types';
 import queries from '#/api/queries';
 
-interface ContactResult {
-  dataset: { value: { contact: [ContactType] } };
-}
-
 interface DatasetQuery {
-  data?: { eventSearch: { documents: { results: [ContactResult] } } };
+  data?: {
+    eventSearch: {
+      documents: {
+        results: [
+          {
+            dataset: {
+              value: {
+                title: string;
+                contact: [ContactType];
+              };
+            };
+          },
+        ];
+      };
+    };
+  };
 }
 
 // Helper function to get the initials of an organisation string
@@ -22,18 +33,11 @@ const getOrgInitials = (org: string) =>
     .slice(0, 3)
     .join('');
 
-interface ContactItemProps {
-  icon: ReactElement;
-}
-
-function ContactItem({ icon, children }: PropsWithChildren<ContactItemProps>) {
-  return (
-    <Group spacing='xs'>
-      {icon}
-      {children}
-    </Group>
-  );
-}
+const contactItemStyle: CSSProperties = {
+  marginRight: 12,
+  minWidth: 24,
+  minHeight: 24,
+};
 
 interface ContactProps {
   dataResource: string;
@@ -41,8 +45,12 @@ interface ContactProps {
 
 function Contact({ dataResource }: ContactProps) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const { data: dataRaw } = useGQLQuery<DatasetQuery>(queries.QUERY_DATASET, { key: dataResource });
-  const contact = dataRaw?.data?.eventSearch.documents.results[0]?.dataset?.value?.contact?.[0];
+  const { data: response } = useGQLQuery<DatasetQuery>(queries.QUERY_DATASET, {
+    key: dataResource,
+  });
+  console.log(response);
+  const dataset = response?.data?.eventSearch.documents.results[0]?.dataset;
+  const contact = dataset?.value?.contact?.[0];
 
   return (
     <Paper withBorder p='md'>
@@ -67,44 +75,53 @@ function Contact({ dataResource }: ContactProps) {
               {contact?.positionName && <Badge radius='sm'>{contact.positionName[0]}</Badge>}
             </Group>
           </Skeleton>
-          <Stack spacing={6}>
-            {contact?.phone && (
-              <ContactItem icon={<IconPhone />}>
-                <Anchor size='sm' href={`tel:${contact.phone[0].replace(' ', '')}`}>
-                  {contact?.phone[0]}
-                </Anchor>
-              </ContactItem>
-            )}
+          <Stack spacing={8}>
             {contact?.electronicMailAddress && (
-              <ContactItem icon={<IconMail />}>
-                <Anchor
-                  size='sm'
-                  href={`mailto:${contact.electronicMailAddress[0].replace(' ', '')}`}
-                >
-                  {contact?.electronicMailAddress[0]}
-                </Anchor>
-              </ContactItem>
+              <Anchor
+                style={{ display: 'flex', alignItems: 'center' }}
+                size='sm'
+                href={`mailto:${contact.electronicMailAddress[0].replace(' ', '')}`}
+              >
+                <IconMail style={contactItemStyle} />
+                {contact?.electronicMailAddress[0]}
+              </Anchor>
+            )}
+            {contact?.phone && (
+              <Anchor
+                style={{ display: 'flex', alignItems: 'center' }}
+                size='sm'
+                href={`tel:${contact.phone[0].replace(' ', '')}`}
+              >
+                <IconPhone style={contactItemStyle} />
+                {contact?.phone[0]}
+              </Anchor>
             )}
             {contact?.address && (
-              <ContactItem icon={<IconMapPin />}>
-                <Text size='sm'>
-                  {[
-                    contact.address[0].deliveryPoint?.[0],
-                    contact.address[0].city?.[0],
-                    contact.address[0].administrativeArea?.[0],
-                    contact.address[0].postalCode?.[0],
-                  ]
-                    .filter((part) => part !== null)
-                    .join(', ')}
-                </Text>
-              </ContactItem>
+              <Text style={{ display: 'flex', alignItems: 'center' }} size='sm'>
+                <IconMapPin style={contactItemStyle} />
+                {[
+                  contact.address[0].deliveryPoint?.[0],
+                  contact.address[0].city?.[0],
+                  contact.address[0].administrativeArea?.[0],
+                  contact.address[0].postalCode?.[0],
+                ]
+                  .filter((part) => part !== null)
+                  .join(', ')}
+              </Text>
             )}
+            <Skeleton visible={!dataset}>
+              <Text size='sm' style={{ display: 'flex', alignItems: 'center' }}>
+                <IconBuilding size={24} style={contactItemStyle} />
+                {dataset?.value?.title}
+              </Text>
+            </Skeleton>
             {!contact &&
               [0, 1].map((id) => (
                 <Skeleton key={id}>
-                  <ContactItem icon={<IconMapPin />}>
-                    <Text size='sm'>Contact information here</Text>
-                  </ContactItem>
+                  <Text size='sm'>
+                    <IconMapPin style={contactItemStyle} />
+                    Contact information here
+                  </Text>
                 </Skeleton>
               ))}
           </Stack>
