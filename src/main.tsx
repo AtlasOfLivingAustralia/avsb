a9b169e86f672ba47a891ea29773276a4037febf
Jack Brinkman <jack.brinkman@csiro.au>
2023-06-15 14:51:37 +1000

    Added axe-core for accessibility testing

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom/client';
 // LogRocket
 import LogRocket from 'logrocket';
 
+// Axe-core accessibility
+import axe from '@axe-core/react';
+
 import Providers from './Providers';
 import Routes from './Routes';
 import './index.css';
@@ -13,6 +16,9 @@ const { VITE_APP_LOGROCKET_ENABLED, VITE_APP_LOGROCKET_ID } = import.meta.env;
 // Initialize LogRocket
 if (VITE_APP_LOGROCKET_ENABLED === 'true' && VITE_APP_LOGROCKET_ID)
   LogRocket.init(VITE_APP_LOGROCKET_ID);
+
+// Initialize axe-core for development
+if (import.meta.env.DEV) axe(React, ReactDOM, 1000);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
