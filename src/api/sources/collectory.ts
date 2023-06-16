f32612d4ae100522f0e412686a9a581619fdbf79
Jack Brinkman <jack.brinkman@csiro.au>
2023-06-16 14:04:41 +1000

    Better 404 handling

--- src/api/sources/collectory.ts
+++ src/api/sources/collectory.ts
@@ -26,7 +26,13 @@ interface DataResourceSummary {
 }
 
 async function dataResource(id: string): Promise<DataResource> {
-  return (await fetch(`${import.meta.env.VITE_API_ALA}/metadata/ws/dataResource/${id}`)).json();
+  const response = await fetch(`${import.meta.env.VITE_API_ALA}/metadata/ws/dataResource/${id}`);
+  const data = await response.text();
+
+  // Catch 200 responses, but no entry found
+  if (response.ok && data.startsWith('no entity with')) throw new Response(data, { status: 404 });
+
+  return JSON.parse(data);
 }
 
 async function dataResourceList(): Promise<DataResourceSummary[]> {
