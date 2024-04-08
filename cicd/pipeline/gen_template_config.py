3ffa6fb513cd9a0577b5c7178d82160e046cb435
Joe Lipson <joe.lipson@csiro.au>
2024-04-08 15:46:50 +1000

    simplify template config

--- cicd/pipeline/gen_template_config.py
+++ cicd/pipeline/gen_template_config.py
@@ -20,22 +20,11 @@ template_loader = jinja2.FileSystemLoader(searchpath="./")
 template_env = jinja2.Environment(loader=template_loader)
 template = template_env.get_template(args.template)
 
-# get the values from the environment
-template_vals = {
-    "allow_listed_ips"  : os.environ['ALLOW_LISTED_IPS'],
-    "branch"            : os.environ['SRC_BRANCH'],
-    "bucket_name"       : os.environ['SOURCE_BUCKET'],
-    "bucket_path"       : os.environ['BUCKET_PATH'],
-    "build"             : os.environ['CODEBUILD_BUILD_NUMBER'],
-    "clean_branch"      : os.environ['CLEAN_BRANCH'],
-    "commit_id"         : os.environ['COMMIT_ID'],
-    "environment"       : os.environ['ENVIRONMENT'],
-    "hosted_zone"       : os.environ['HOSTED_ZONE'],
-    "product_component" : os.environ['PRODUCT_COMPONENT'],
-    "product_name"      : os.environ['PRODUCT_NAME'],
-    "ssl_certificate"   : os.environ['SSL_CERTIFICATE'],
-    "sub_domain"        : os.environ['SUB_DOMAIN'],
-}
+template_vals = {}
+
+# template values are created as lower case versions of environment variables
+for key, value in os.environ.items():
+    template_vals[key.lower()] = value
 
 # do the substitution
 output_text = template.render(template_vals)
