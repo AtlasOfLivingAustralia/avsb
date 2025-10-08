7f468a106fe5125d3f64c4f6ff248ce5d437d345
Jack Brinkman <jack.brinkman@csiro.au>
2025-10-09 09:08:02 +1000

    Bump Vite & related testing dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import { visualizer } from 'rollup-plugin-visualizer';
-import pluginRewriteAll from 'vite-plugin-rewrite-all';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), pluginRewriteAll(), tsconfigPaths(), visualizer()],
+  plugins: [react(), tsconfigPaths(), visualizer()],
   envDir: './config',
 });
