a79f6927ad37b9c66efe223a8ee71ddfdb48d932
Jack Brinkman <jack.brinkman@csiro.au>
2026-05-05 15:09:54 +1000

    Update vite config to reflect v8 changes, minor dependency bump

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
 // import react from '@vitejs/plugin-react-swc';
-import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import babel from '@rolldown/plugin-babel';
+// import { analyzer, unstableRolldownAdapter } from 'vite-bundle-analyzer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      },
-    }),
-    tsconfigPaths(),
-    visualizer(),
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    // unstableRolldownAdapter(analyzer())
   ],
+  resolve: {
+    tsconfigPaths: true
+  },
   envDir: './config',
 });
